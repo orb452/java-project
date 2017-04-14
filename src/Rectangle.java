78978ccfdf0ae9bd43a1384bc9201c29c3287f2e
root <root@localhost.localdomain>
2017-04-14 12:02:35 +0530

    Added Junit good test cases

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,12 +1,10 @@
 public class Rectangle {
   public int length;
   public int width;
->>>>>>>>>>>Head
   public Rectangle(int length, int width) {
     this.length = length;
     this.width = width;
   }
->>>>>>>>>>>>77899999999999999999999999999
   public int getArea() {
     return length * width;
   }
